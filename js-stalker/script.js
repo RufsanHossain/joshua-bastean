5bf915e63b3d45aa205abf1b8983d5e8b9f21b93
Rufsan Hossain Santo <rufsanhossain315@gmail.com>
2024-02-20 11:00:07 +0600

    add JS Stalker

---
+++ js-stalker/script.js
@@ -0,0 +1,17 @@
+// Declare variables, asking for user input
+
+const username = prompt("What is your name?");
+const age = prompt("What is your age?");
+const hobby = prompt("What is your hobby?");
+
+// Create sentence and save to a new variable
+
+let info = "You are " + username + ". You are " + age + " years old." + "You love " + hobby + ".";
+
+// Console log
+
+console.log(info);
+
+// Alert 
+
+alert(info);
